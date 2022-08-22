d0408b2d7d0c77bb1338fad24f799d35fda5f0aa
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-22 06:35:36 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
   # your code here
+
+    def initialize(attributes)
+
+      attributes.each do |key,value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=",value)
+      end
+
+    end
+
 end
+
+person1_attributes = {name:"Martin Kamau", hair_color: "Black",eye_color: "Brown",height: 5.4,weight:"58kgs",handed:"right-handed",complexion:"Brown",t_shirt_size:"Large",wrist_size:"7cm radius",glove_size:"Medium Palm",pant_length:"Hard to remember",pant_width: "32cm radius"}
+
+pp person1 = Person.new(person1_attributes)
